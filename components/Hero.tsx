b82202c75de4f1713e64b8f8a7fbdd0457703674
sonnmGemJpn <sonnm@gemjpn.com>
2023-10-20 00:36:44 +0900

    fix navbar and css

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,6 +15,7 @@ const Hero = () => {
 
   return (
     <div className="hero">
+
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
           Find, book, rent a hotel and restaurant super easy!
@@ -31,12 +32,27 @@ const Hero = () => {
           handleClick={handleScroll}
         />
       </div>
-      <div className="hero__image-container">
-        <div className="hero__image">
-          <Image src="/Sapa1.jpg" alt="hero" fill className="object-contain" />
+      <div className="relative shadow-xl sm:overflow-hidden">
+        <div className="absolute inset-0">
+          {/* Replace with your image source */}
+          <Image
+            priority
+            fill
+            className="h-full w-full object-cover"
+            src="/Sapa1.jpg"
+            alt="explore"
+          />
+          <div className="absolute inset-0 bg-orange-100 mix-blend-multiply" />
         </div>
-
-        {/* <div className="hero__image-overlay" /> */}
+        <div className="relative px-4 py-16 sm:px-6 sm:py-24 lg:py-32 lg:px-8">
+          <p className="relative left-0 right-0 mx-auto mt-5 max-w-xl text-center text-xl font-semibold uppercase tracking-wide text-orange-600">
+            Easy way to explore the world
+          </p>
+          <h1 className="mt-1 text-center font-bold uppercase text-gray-900 sm:text-5xl sm:tracking-tight lg:text-7xl">
+            <span className="block text-white">Life is better with</span>
+            <span className="block text-orange-500">travel</span>
+          </h1>
+        </div>
       </div>
     </div>
   );
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -6,24 +6,33 @@ import CustomButton from "./CustomButton";
 const NavBar = () => (
   <header className='w-full  absolute z-10'>
     <nav className='max-w-[1440px] mx-auto flex justify-between items-center sm:px-16 px-6 py-4 bg-transparent'>
-      <Link href='/' className='flex justify-center items-center'>
-        <Image
-          src='/backpacklg.svg'
-          alt='logo'
-          width={118}
-          height={18}
-          className='object-contain'
-        />
-      </Link>
-      <Link href='/aboutUs'  className='flex justify-center items-center' >
-        About Us
-      </Link>
+      <div className="justify-between-full fixed z-40 bg-[#fff] w-full left-0 top-0">
+        <div className="flex flex-auto items-center mx-auto px-6 xl:container pt-3 pb-2">
+          <div className="flex mx-auto flex-auto justify-between items-center relative">
+            <div className="flex gap-4 md:gap-10 items-center">
+              <Link href='/' className='flex justify-center items-center'>
+                <Image
+                  src='/backpacklg.svg'
+                  alt='logo'
+                  width={118}
+                  height={18}
+                  className='object-contain'
+                />
+              </Link>
+              <Link href='/aboutUs' className='flex justify-center items-center' >
+                About Us
+              </Link>
+              <CustomButton
+                title='Sign in'
+                btnType='button'
+                containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
+              />
+            </div>
+          </div>
+        </div>
+      </div>
 
-      <CustomButton
-        title='Sign in'
-        btnType='button'
-        containerStyles='text-primary-blue rounded-full bg-white min-w-[130px]'
-      />
+
     </nav>
   </header>
 );
